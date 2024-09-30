dfe59116817685e4253c786acd9e99d68ea9a126
yveso <yveso@users.noreply.github.com>
2024-09-30 21:26:10 +0200

    Initial 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Hockey Punkte", page_icon="🏒")
+
+st.title("🏒Hockey Punkte🏒")
+st.write("Coming soon...")
